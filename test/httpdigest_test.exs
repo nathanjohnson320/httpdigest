5a9367041ed271b8ae7c7d5c470e134102928e42
Nathan Johnson <najohnson@redventures.com>
2018-06-29 09:18:49 -0400

    Adding tests

--- test/httpdigest_test.exs
+++ test/httpdigest_test.exs
@@ -2,7 +2,28 @@ defmodule HttpdigestTest do
   use ExUnit.Case
   doctest Httpdigest
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "add_opaque(response, [nil, \"\"])" do
+    res =
+      Httpdigest.create_response("iZZZZZZ", "test", "GET", "/", %{
+        "realm" => "API",
+        "nonce" => "MTUzMDI2OTExOTo1ZmYwODVlNzY0MTM5OWVmZWI3M2NiZmNkZTdmYzQ5YQ==",
+        "qop" => "auth"
+      })
+
+    # Shouldn't have an opaque field
+    assert not String.contains?(res, "opaque")
+  end
+
+  test "add_opaque(response, \"b816d4d26130bed8ccf5149e855000ad\")" do
+    res =
+      Httpdigest.create_response("iZZZZZZ", "test", "GET", "/", %{
+        "realm" => "API",
+        "nonce" => "MTUzMDI2OTExOTo1ZmYwODVlNzY0MTM5OWVmZWI3M2NiZmNkZTdmYzQ5YQ==",
+        "qop" => "auth",
+        "opaque" => "b816d4d26130bed8ccf5149e855000ad"
+      })
+
+    # Should have an opaque field
+    assert String.contains?(res, "opaque")
   end
 end
