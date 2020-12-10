efe782d7defab9af070d8d97204bd1e048c61626
Nathan Johnson <nathanjohnson320@gmail.com>
2020-12-10 08:09:58 -0500

    0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Httpdigest.Mixfile do
   def project() do
     [
       app: :httpdigest,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
